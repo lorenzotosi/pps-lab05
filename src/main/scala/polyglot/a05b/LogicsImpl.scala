f6ef0b6f34d1ce3392a54e6b3eaabd65aef3ebff
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2025-04-02 13:51:18 +0200

    ex 2

--- src/main/scala/polyglot/a05b/LogicsImpl.scala
+++ src/main/scala/polyglot/a05b/LogicsImpl.scala
@@ -4,8 +4,28 @@ import polyglot.a05b.Logics
 
 /** solution and descriptions at https://bitbucket.org/mviroli/oop2019-esami/src/master/a05b/sol2/ */
 class LogicsImpl(private val size: Int) extends Logics:
-  override def tick(): Unit = {}
+  private var tickCount = 0
+  private val random = scala.util.Random(10)
+  private val initial: (Int, Int) = (random.nextInt(size - 2) + 1, random.nextInt(size - 2) + 1)
 
-  override def isOver: Boolean = false
+  override def tick(): Unit = tickCount = tickCount + 1
 
-  override def hasElement(x: Int, y: Int): Boolean = false
+  override def isOver: Boolean =
+    (initial._2 - tickCount < 0) || (initial._2 + tickCount >= size) ||
+      (initial._1 - tickCount < 0) || (initial._1 + tickCount >= size)
+
+//    initial match
+//    case (x, y) => (y - tickCount < 0) || (y + tickCount >= size) || (x - tickCount < 0) || (x + tickCount >= size)
+
+  override def hasElement(x: Int, y: Int): Boolean =
+    (x == initial._1) && (Math.abs(y - initial._2) <= tickCount) ||
+      (y == initial._2) && (Math.abs(x - initial._1) <= tickCount) ||
+      (x - y == initial._1 - initial._2) && (Math.abs(x - initial._1) <= tickCount) ||
+      (x + y == initial._1 + initial._2) && (Math.abs(x - initial._1) <= tickCount)
+
+//    initial match
+//    case (initialX, initialY) => (x == initialX) && (Math.abs(y - initialY) <= tickCount) ||
+//      (y == initialY) && (Math.abs(x - initialX) <= tickCount) ||
+//      (x - y == initialX - initialY) && (Math.abs(x - initialX) <= tickCount) ||
+//      (x + y == initialX + initialY) && (Math.abs(x - initialX) <= tickCount)
+
